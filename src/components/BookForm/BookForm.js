d63d613b0cb3911a821173ba98c79bb18c4eca90
itsokaytobesergey <by.imbooo@gmail.com>
2024-02-19 23:58:08 +0500

    init commit
    full application

---
+++ src/components/BookForm/BookForm.js
@@ -0,0 +1,63 @@
+import { useState } from 'react';
+import { useDispatch } from 'react-redux';
+import { addBook } from '../../redux/slices/booksSlice';
+import { setError } from '../../redux/slices/errorSlice';
+import createBookWithID from '../../utils/createBookWithID';
+import booksData from '../../data/books.json';
+import './BookForm.css';
+
+const BookForm = () => {
+    const [title, setTitle] = useState('');
+    const [author, setAuthor] = useState('');
+    const dispatch = useDispatch();
+
+    const handleAddRandomBook = () => {
+        const randomIndex = Math.floor(Math.random() * booksData.length);
+        const randomBook = booksData[randomIndex];
+        dispatch(addBook(createBookWithID(randomBook, 'random')));
+    };
+
+    const handleSubmit = (e) => {
+        e.preventDefault();
+
+        if (title && author) {
+            dispatch(addBook(createBookWithID({ title, author }, 'manual')));
+            setTitle('');
+            setAuthor('');
+        } else {
+            dispatch(setError('You must fill title and author!'));
+        }
+    };
+
+    return (
+        <div className="app-block book-form">
+            <h2>Add a New Book</h2>
+            <form onSubmit={handleSubmit}>
+                <div>
+                    <label htmlFor="title">Title: </label>
+                    <input
+                        type="text"
+                        id="title"
+                        value={title}
+                        onChange={(e) => setTitle(e.target.value)}
+                    />
+                </div>
+                <div>
+                    <label htmlFor="author">Author: </label>
+                    <input
+                        type="text"
+                        id="author"
+                        value={author}
+                        onChange={(e) => setAuthor(e.target.value)}
+                    />
+                </div>
+                <button type="submit">Add Book</button>
+                <button type="button" onClick={handleAddRandomBook}>
+                    Add Random
+                </button>
+            </form>
+        </div>
+    );
+};
+
+export default BookForm;
